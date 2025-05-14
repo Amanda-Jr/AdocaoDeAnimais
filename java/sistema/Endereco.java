81df3c09ece157af0b8e30622debd512027b21e3
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2025-05-14 19:10:39 -0300

    atributos

--- java/sistema/Endereco.java
+++ java/sistema/Endereco.java
@@ -2,4 +2,46 @@ package java.sistema;
 
 public class Endereco {
     
+    private int idEndereço;
+    private String rua;
+    private String bairro;
+    private String cep;
+    private String numero;
+
+
+    public int getIdEndereço() {
+        return idEndereço;
+    }
+    public void setIdEndereço(int idEndereço) {
+        this.idEndereço = idEndereço;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+
+
+
 }
