0f9e8ead406cce3a11205e2fa13ce7ff660ab32f
weep_dev <almeidapereirast@gmail.com>
2025-05-14 18:25:48 -0300

    root

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './pages/Cadastro/index.css'
+import { BrowserRouter, Routes, Route } from "react-router";
 import Cadastro from './pages/Cadastro/App.tsx'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Cadastro />
+    <BrowserRouter>
+      <Routes>
+      <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
