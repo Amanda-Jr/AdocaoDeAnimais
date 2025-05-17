fbfd51db9f0785044cd25f8f81458bc3cfce8cda
sstbiaanca <144484262+sstbiaanca@users.noreply.github.com>
2025-05-17 16:52:14 -0300

    Criar Status.java

---
+++ java/sistema/Status.java
@@ -0,0 +1,16 @@
+public class Status {
+
+    private boolean disponivel;
+
+    public Status(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
