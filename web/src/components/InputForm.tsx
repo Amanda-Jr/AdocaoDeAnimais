5277869260b75b902bb303704f4ad17241d862af
weep_dev <almeidapereirast@gmail.com>
2025-05-17 19:34:44 -0300

    add tela cadastro

--- web/src/components/InputForm.tsx
+++ web/src/components/InputForm.tsx
@@ -3,6 +3,10 @@ import { twMerge } from "tailwind-merge";
 
 type InputFormProps = HTMLProps<HTMLInputElement>
 
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  label: string;
+};
+
 export function InputForm(props: InputFormProps) {
   return (
     <div className="flex flex-col font-medium">
@@ -14,5 +18,19 @@ export function InputForm(props: InputFormProps) {
         className={twMerge("py-3 px-5 bg-[#E5E5E5] rounded-[6px] placeholder:text-black outline-0 mt-[0.25rem]", props.className)}
       />
     </div>
+  );
+}
+
+export function Button({ label, className, ...rest }: ButtonProps) {
+  return (
+    <button
+      {...rest}
+      className={twMerge(
+        "bg-[#C78E00] w-3xs text-white font-medium py-5 px-6 rounded-[6px] transition-colors duration-200 hover:bg-[#a97600] disabled:opacity-50 mt-7 self-center",
+        className
+      )}
+    >
+      {label}
+    </button>
   );
 }
--- web/src/pages/Cadastro/App.tsx
+++ web/src/pages/Cadastro/App.tsx
@@ -1,9 +1,9 @@
-import { InputForm } from "@/components/InputForm";
+import { Button, InputForm } from "@/components/InputForm";
 
 export default function Cadastro() {
   return (
     <div className="cadastro">
-      <form className="p-5 flex flex-col min-w-3/4">
+      <form className="p-5 flex flex-col min-w-2/4">
         <h1 className="text-center font-medium text-[1.75rem]">Cadastrar</h1>
         <p className="text-center font-medium text-[1.25rem]">
           Criar nova conta
@@ -14,7 +14,16 @@ export default function Cadastro() {
           id="username"
         />
         <InputForm label="Email" placeholder="Email" id="email"/>
+        <InputForm label="Numero de Telefone" placeholder="Digite o seu numero de Telefone" id="tel"/>
+        <InputForm label="Senha" placeholder="Digite sua senha" id="password"/>
+        <Button label="Cadastrar"/>
+        <p className="pl-5 text-start text-[1.2rem]">Ja tem uma conta?  <Button className="bg-white text-[#091E3E] w-1xs border-4 border-[#C78E00]" label="Cadastrar"/></p>
+        
       </form>
+      <img className="h-auto max-w-md ms-auto" src="./src/images/cadastro/gatoCadastro.png" alt="image description"></img>
+      
     </div>
+    
   );
+  
 }
