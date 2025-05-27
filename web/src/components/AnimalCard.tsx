1da34297eade308f8f6da2d2b28d45e4ad88593a
yngridAMatos <matosingridy76@gmail.com>
2025-05-27 15:05:35 -0300

    integra o back e altera o AnimalCard para receber os dados do banco

--- web/src/components/AnimalCard.tsx
+++ web/src/components/AnimalCard.tsx
@@ -1,20 +1,62 @@
+import React from "react";
 import { Mars, Venus } from "lucide-react";
 
-//<Venus className="text-[#FF56EE]" />
+export type Animal = {
+    id: number;
+    animal: "dogs" | "cats";
+    size: "small" | "medium" | "large";
+    age: "puppy" | "adult";
+    location: string;
+    name: string;
+    image: string;
+};
 
-export function AnimalCard() {
-  return (
-    <div className="text-center text-black bg-[#F2F2F2] max-w-min max-h-min rounded-2xl overflow-hidden hover:scale-105 transition-transform duration-500">
-      <img src="/animal-card-example.png" alt="Imagem do animal" className="cursor-pointer" />
-      <div className="py-3 px-7 text-[0.9rem]">
-        <p className="font-extrabold text-nowrap text-[1.2rem]">
-          Rubinho <Mars className="text-[#299EF3] inline" />
-        </p>
-        <p className="text-nowrap">
-          Porte Médio <span>●</span> Filhote
-        </p>
-        <p>São Paulo</p>
-      </div>
-    </div>
-  );
+export function AnimalCard({
+                               animal,
+                               size,
+                               age,
+                               location,
+                               name,
+                               image,
+                           }: Animal) {
+    const displaySize: Record<string, string> = {
+        small: "Pequeno",
+        medium: "Médio",
+        large: "Grande",
+    };
+
+    const displayAge: Record<string, string> = {
+        puppy: "Filhote",
+        adult: "Adulto",
+    };
+
+    const icon =
+        animal === "dogs" ? (
+            <Mars className="text-[#299EF3] inline" />
+        ) : (
+            <Venus className="text-[#FF56EE] inline" />
+        );
+
+    return (
+        <div
+            className="text-center text-black bg-[#F2F2F2] max-w-min max-h-min rounded-2xl overflow-hidden hover:scale-105 transition-transform duration-500">
+            <img
+                src={image}
+                alt={`Imagem de ${name}`}
+                onError={(e) => {
+                    e.currentTarget.src = "/fallback-image.png";
+                }}
+                className="cursor-pointer"
+            />
+            <div className="py-3 px-7 text-[0.9rem]">
+                <p className="font-extrabold whitespace-nowrap text-[1.2rem]">
+                    {name} {icon}
+                </p>
+                <p className="whitespace-nowrap">
+                    {displaySize[size]} <span>●</span> {displayAge[age]}
+                </p>
+                <p>{location}</p>
+            </div>
+        </div>
+    );
 }
